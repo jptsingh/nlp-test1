5db93f20325a11cf13338b8f4b059a1790ab0c2c
jptsingh <67818421+jptsingh@users.noreply.github.com>
2021-05-14 00:13:08 -0700

    Add files via upload

---
+++ FEND Capstone - Travel App V2.0/src/client/index.js
@@ -0,0 +1,22 @@
+import { performActionGeoname } from './js/app'
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+//import './styles/resets.scss'
+//import './styles/base.scss'
+//import './styles/footer.scss'
+//import './styles/form.scss'
+//import './styles/header.scss'
+import './styles/style.scss'
+
+console.log(checkForName);
+console.log(performActionGeoname);
+
+alert("I EXIST")
+console.log("CHANGE!!");
+
+export {
+    performActionGeoname,
+    checkForName,
+    handleSubmit
+}
